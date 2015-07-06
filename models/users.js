8d104c6365633abc6278cde4378f60bd6bdd3e00
John Alejandro Mantilla Celis <alejandromantilla7@hotmail.com>
2015-07-05 23:32:50 -0500

    Edited user model adding pre-save validation, and verify passwords

--- models/users.js
+++ models/users.js
@@ -1,12 +1,19 @@
 var mongoose = require('mongoose');
+var bcrypt = require('bcrypt-nodejs');
 var Schema = mongoose.Schema;
 
-var usersSchema = new Schema({
+var userSchema = new Schema({
     name : String,
     lastName : String,
-    Password : String,
-    salt : String,
-    email : String,
+    email : {
+        type : String,
+        unique : true,
+        required : true
+    },
+    password : {
+        type : String,
+        required : true
+    },
     photo : String,
     birthday : Date,
     idCountry : Schema.Types.ObjectId,
@@ -29,4 +36,35 @@ var usersSchema = new Schema({
     enabled : Boolean
 },{ versionKey: false });
 
-module.exports = mongoose.model('Users', usersSchema);
+// Execute before each user.save() call
+userSchema.pre('save', function (callback) {
+    var user = this;
+
+    // Break out if the password hasn't changed
+    if(!user.isModified('password')) return callback();
+
+    // Password changed so we need to hash it
+    bcrypt.genSalt(10, function(err, salt) {
+        if(err) return callback(err);
+
+        bcrypt.hash(user.password, salt, null, function (err, hash) {
+            if(err) return callback(err);
+            user.password = hash;
+            callback();
+        });
+    });
+});
+
+// Verification methods
+userSchema.methods.verifyPassword = function (password, callback) {
+    bcrypt.compare(password, this.password, function (err, isMatch) {
+        if(err){
+            return callback(err);
+        }
+        callback(null, isMatch);
+    });
+};
+
+
+// Export the mongoose model
+module.exports = mongoose.model('Users', userSchema);
