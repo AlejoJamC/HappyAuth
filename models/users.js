b53abd19504fc7f7254106e9480392810bcb169d
John Alejandro Mantilla Celis <alejandromantilla7@hotmail.com>
2015-07-05 20:25:27 -0500

    Updated user data model

--- models/users.js
+++ models/users.js
@@ -2,7 +2,31 @@ var mongoose = require('mongoose');
 var Schema = mongoose.Schema;
 
 var usersSchema = new Schema({
-
-}, { versionKey: false });
+    name : String,
+    lastName : String,
+    Password : String,
+    salt : String,
+    email : String,
+    photo : String,
+    birthday : Date,
+    idCountry : Schema.Types.ObjectId,
+    country : String,
+    isoCountryCode : String,
+    telephones : [{
+        idCountryCodePhone : Schema.Types.ObjectId,
+        CountryCodePhone : String,
+        phone : String
+    }],
+    addresses : [
+        {
+            address : String,
+            country : String,
+            state : String,
+            city : String,
+            zip : String
+        }
+    ],
+    enabled : Boolean
+},{ versionKey: false });
 
 module.exports = mongoose.model('Users', usersSchema);
